6e9c96b064de940b58fb4208df2545450bca651c
utkarsh29081997 <38398744+utkarsh29081997@users.noreply.github.com>
2020-11-07 20:24:23 +0530

    Update CheckDirectory.py

--- CheckDirectory.py
+++ CheckDirectory.py
@@ -5,16 +5,9 @@ import CreateFolder
 # Getting or Changing Directory
 def check_directory():
     # Getting Path current Folder and changing it to desktop if it's  not
+    get_desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+    os.chdir(get_desktop)
     current_dir = os.getcwd()
-    list_first = current_dir.split("/")
-
-    # Find weather we are in Desktop folder or not
-    if "Desktop" not in list_first:
-        os.chdir("C:/Users/user/Desktop")
-        change_directory = os.getcwd()
-        print(change_directory)
-        CreateFolder.create_folder()
-    else:
-        CreateFolder.create_folder()
+    CreateFolder.create_folder()
 
 
