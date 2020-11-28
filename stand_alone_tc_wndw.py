3bf818845641178a1527ce525e9dd1b2bf2a63a0
utkarsh29081997 <38398744+utkarsh29081997@users.noreply.github.com>
2020-11-29 00:39:27 +0530

    Update stand_alone_tc_wndw.py

--- stand_alone_tc_wndw.py
+++ stand_alone_tc_wndw.py
@@ -7,6 +7,7 @@ import os
 from tkinter import *
 from tkinter import messagebox, simpledialog
 from tkinter import ttk
+import Utility_Launch
 
 from CheckDirectory import check_directory
 from scrnshot_wndw import scrnshot
@@ -17,34 +18,41 @@ from testcase_wndw import name_of_testcase
 class tc_window(Toplevel):
     def __init__(self):
         Toplevel.__init__(self)
-        self.geometry("550x550+700+120")
+        self.geometry("550x550+790+120")
         self.title("One Stop Window")
         self.resizable(False, False)
+
         # Top Frame for icon and name
         self.frame_on_standalone = Frame(self, height=120, bg="white")
         self.frame_on_standalone.pack(fill="both")
-        self.top_framestand_alone = PhotoImage(file='S:/Projects/Python Projects/Utility/Image_01/boy.png')
-        self.label_photo = ttk.Label(self.frame_on_standalone, image=self.top_framestand_alone,
-                                     background="white").place(x=30, y=30)
+        try:
+            self.top_framestand_alone = PhotoImage(file=os.path.join(Utility_Launch.path, 'Image_01/boy.png'))
+            self.label_photo = ttk.Label(self.frame_on_standalone, image=self.top_framestand_alone,
+                                         background="white").place(x=30, y=30)
+        except:
+            pass
         self.label_heading = ttk.Label(self.frame_on_standalone, text="Get Done!!!", font=("Times New Roman", 15)
                                        , background="white").place(x=190, y=40)
-
-        # Take Application name from drop down
-        self.frame = Frame(self, height=530, bg='#c99a0c').pack(fill='both')
-        self.get_app_list_from_env()
-        print(os.getcwd(), "Here In standalone")
-        self.get_app_name = StringVar(self)
-        list_of_env = os.listdir()
-        self.drp_dowlist_env = ttk.Combobox(self, width=27, textvariable=self.get_app_name, height=15, state="readonly")
-        self.drp_dowlist_env['values'] = list_of_env
-        self.drp_dowlist_env.place(x=50, y=130)
-        self.drp_dowlist_env.set('Choose a File')
-        self.create_btn1 = ttk.Button(self, width=14, text='      OK      ', command=self.app_name_for_testcase)
-        self.create_btn1.place(x=50, y=184)
-        self.pick_last = ttk.Button(self, width=14, text='Pick Last Flow', command=self.call_lastpick)
-        self.pick_last.place(x=50, y=284)
-        self.quit_app = ttk.Button(self, width=15, text='    Close    ', command=self.close_wndw)
-        self.quit_app.place(x=175, y=184)
+        # If no Folder exists in Screenshot it will destroy the current window without any exception
+        try:
+            # Take Application name from drop down
+            self.frame = Frame(self, height=530, bg='#c99a0c').pack(fill='both')
+            self.get_app_list_from_env()
+            self.focus()
+            self.get_app_name = StringVar(self)
+            list_of_env = os.listdir()
+            self.drp_dowlist_env = ttk.Combobox(self,width=27,textvariable=self.get_app_name,height=15,state="readonly")
+            self.drp_dowlist_env['values'] = list_of_env
+            self.drp_dowlist_env.place(x=50, y=130)
+            self.drp_dowlist_env.set('Choose a File')
+            self.create_btn1 = ttk.Button(self, width=14, text='      OK      ', command=self.app_name_for_testcase)
+            self.create_btn1.place(x=50, y=184)
+            self.pick_last = ttk.Button(self, width=14, text='Pick Last Flow', command=self.call_lastpick)
+            self.pick_last.place(x=50, y=284)
+            self.quit_app = ttk.Button(self, width=15, text='    Close    ', command=self.close_wndw)
+            self.quit_app.place(x=175, y=184)
+        except:
+            pass
 
     def close_wndw(self):
         self.destroy()
@@ -53,51 +61,44 @@ class tc_window(Toplevel):
     def get_app_list_from_env(self):
         check_directory()
         response = messagebox.askquestion("!nfo", "Choose YES for a specific Environment and NO to move in General")
-        print(os.getcwd())
         # Folders which are inside Screenshot
         folder_in_app = os.listdir()
-        if response == 'yes':
-            print(os.getcwd(), "get app env")
-            env_question = simpledialog.askstring("Environment!", "Enter Environment (Case Sensitive)")
-            if env_question in folder_in_app:
-                os.chdir(os.path.join(os.getcwd(), env_question))
-                simpledialog.messagebox.showinfo("Info!", 'Inside {} Environment Folder'.format(env_question))
-            elif env_question not in folder_in_app:
-                simpledialog.messagebox.showinfo("Error!", '{}Environment Folder does not exist'.format(env_question))
-                self.get_app_list_from_env()
-        elif response == 'no':
-            os.chdir(os.path.join(os.getcwd(), "General"))
-            print("created", os.getcwd())
+        try:
+            if response == 'yes':
+                env_question = simpledialog.askstring("Environment!", "Enter Environment (Case Sensitive)")
+                if env_question in folder_in_app:
+                    os.chdir(os.path.join(os.getcwd(), env_question))
+                    simpledialog.messagebox.showinfo("Info!", 'Inside {} Environment Folder'.format(env_question))
+                elif env_question not in folder_in_app:
+                    simpledialog.messagebox.showinfo("Error!",
+                                                     '{} Environment Folder does not exist'.format(env_question))
+                    self.get_app_list_from_env()
+            elif response == 'no':
+                os.chdir(os.path.join(os.getcwd(), "General"))
+        except:
+            simpledialog.messagebox.showinfo("Error!", 'No Folder available in Screenshot')
+            self.close_wndw()
 
     # Get application folder from Environment list
     def app_name_for_testcase(self):
         # Change the directory Since it will create the folder in Project File and we donn't need that so changing the
         # folder location to desktop screenshot
-        print(self.get_app_name.get())
         if self.get_app_name.get() != "Choose a File":
-            print(os.getcwd(), "In Sample 1")
-            os.chdir(os.path.join(os.getcwd(), self.get_app_name.get() + "/Hands-on"))
-            print(os.getcwd(), "In Sample 2")
+            os.chdir(os.path.join(os.getcwd(), self.get_app_name.get()))
             name_of_testcase()
             self.close_wndw()
         else:
             messagebox.showerror("Error!", "Please Enter Application name", icon="error")
-            self.close_wndw()
-            return tc_window()
 
     # To recreate last flow
     def call_lastpick(self):
-        self.create_btn1['state'] = DISABLED
-        print(os.getcwd(), "Before")
         # Take App name
-        print(self.get_app_name.get())
         try:
+            self.create_btn1['state'] = DISABLED
+            self.pick_last['state'] = DISABLED
             if self.get_app_name.get() != "Choose a File":
-                print(os.getcwd(), "In Sample 1")
                 app_foldr_name = self.get_app_name.get()
-                os.chdir(os.path.join(os.getcwd(), app_foldr_name) + "/Hands-On")
-                print(os.getcwd(), "In Sample 2")
-
+                os.chdir(os.path.join(os.getcwd(), app_foldr_name))
                 # Take test case name from the list
                 get_tc = StringVar(self)
                 list_of_files = os.listdir()
@@ -113,18 +114,15 @@ class tc_window(Toplevel):
 
             else:
                 messagebox.showerror("Error!", "Please Select Application")
-                self.close_wndw()
-                return tc_window()
+                self.create_btn1['state'] = NORMAL
         except:
             messagebox.showerror("Error!", "Please Select Test Case")
 
     def call_scrnshot_for_lastpick(self, test_case_folder):
         if test_case_folder != "Choose a Test Case":
             self.close_wndw()
-            print(test_case_folder, os.getcwd())
             test = test_case_folder
             os.chdir(os.path.join(os.getcwd(), test))
-            print(os.getcwd(), "Here")
             scrnshot()
         else:
             messagebox.showerror("Error!", "Please Select Test Case")
