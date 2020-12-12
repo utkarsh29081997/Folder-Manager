7fbb307ed1510dd83115fe0175485a30250dfa25
utkarsh29081997 <38398744+utkarsh29081997@users.noreply.github.com>
2020-12-12 20:49:55 +0530

    Update scrnshot_wndw.py

--- scrnshot_wndw.py
+++ scrnshot_wndw.py
@@ -2,7 +2,7 @@
 # Author : Utkarsh Singh
 # Project : Daily Utility
 # Module : Window to take Screenshot and Enter images into the word
-######################################################################
+#######################################################################
 import datetime
 import os
 from tkinter import *
